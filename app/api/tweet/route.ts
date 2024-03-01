3d322baf7fd44ef2d9041d499a34339416ffa3c5
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-02-29 20:22:02 -0500

    twitter api

--- app/api/tweet/route.ts
+++ app/api/tweet/route.ts
@@ -1,26 +1,31 @@
 import { NextResponse } from 'next/server';
+import { TwitterApi } from 'twitter-api-v2';
 
 export async function POST(request: Request) {
     const body = JSON.parse(await request.text());
     const token = body.token;
     const text = body.text;
 
-    const API = 'https://api.twitter.com/2/tweets';
-    const auth = `Bearer ${token}`;
+    // const API = 'https://api.twitter.com/2/tweets';
+    // const auth = `Bearer ${token}`;
 
-    const json = JSON.stringify({ 'text': text });
+    // const json = JSON.stringify({ 'text': text });
 
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Authorization': auth,
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: json
-    };
+    // const requestOptions = {
+    //   method: 'POST',
+    //   headers: {
+    //     'Authorization': auth,
+    //     'Content-Type': 'application/json',
+    //     'Access-Control-Allow-Origin': '*'
+    //   },
+    //   body: json
+    // };
 
-    const response = await fetch(API as string, requestOptions);
-    const data = await response.json();
-    return NextResponse.json(data);
+    // const response = await fetch(API as string, requestOptions);
+    // const data = await response.json();
+    const client = new TwitterApi(token);
+
+    const response = await client.v2.tweet(text);
+
+    return NextResponse.json(response);
 }
