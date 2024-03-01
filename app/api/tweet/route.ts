4be0b2f59ae46542c3b568ec2bc5e3dbddaaea2a
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-03-01 00:49:13 -0500

    twitter api

--- app/api/tweet/route.ts
+++ app/api/tweet/route.ts
@@ -1,23 +1,19 @@
 import { NextResponse } from 'next/server';
-import { TwitterApi } from 'twitter-api-v2';
 
 export async function POST(request: Request) {
     const body = JSON.parse(await request.text());
     const { access_token, secret_token } = body.tokens;
     const text = body.text;
 
-    const client = new TwitterApi({
-        appKey: process.env.NEXT_PUBLIC_TWITTER_API_KEY as string,
-        appSecret: process.env.NEXT_PUBLIC_TWITTER_API_SECRET as string,
-        accessToken: access_token,
-        accessSecret: secret_token
+    const response = await fetch('https://api.twitter.com/1.1/statuses/update.json?status=' + encodeURI(text), {
+        method: 'POST',
+        headers: {
+            'Authorization': `authorization: OAuth oauth_consumer_key=${process.env.TWITTER_API_KEY}, oauth_nonce="OAUTH_NONCE", oauth_signature="OAUTH_SIGNATURE", oauth_signature_method="HMAC-SHA1", oauth_timestamp="OAUTH_TIMESTAMP", oauth_token=${access_token}, oauth_version="1.0"`,
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({ 'text': text })
     });
-
-    console.log(client);
-
-    const rwClient = client.readWrite;
-
-    const response = await rwClient.v2.tweet(text);
 
     return NextResponse.json(response);
 }
