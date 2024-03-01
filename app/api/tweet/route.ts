44ac0ae3b3d776776b376b3bcaaa978475844bf0
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-03-01 00:54:51 -0500

    twitter api

--- app/api/tweet/route.ts
+++ app/api/tweet/route.ts
@@ -12,8 +12,16 @@ export async function POST(request: Request) {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'
         },
-        body: JSON.stringify({ 'text': text })
     });
 
     return NextResponse.json(response);
 }
+/*
+
+curl --request POST \
+
+  --url 'https://api.twitter.com/1.1/statuses/update.json?status=Hello%20world' \
+
+  --header 'authorization: OAuth oauth_consumer_key="CONSUMER_API_KEY", oauth_nonce="OAUTH_NONCE", oauth_signature="OAUTH_SIGNATURE", oauth_signature_method="HMAC-SHA1", oauth_timestamp="OAUTH_TIMESTAMP", oauth_token="ACCESS_TOKEN", oauth_version="1.0"' \
+
+  */
