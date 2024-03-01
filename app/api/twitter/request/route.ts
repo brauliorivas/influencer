27f2d076b6ab71a434a7fb762300ad1573b4e0ec
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-02-29 23:02:14 -0500

    twitter api

---
+++ app/api/twitter/request/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+    const twitter_api = 'https://api.twitter.com/';
+
+    const req_token = await fetch(twitter_api + 'oauth/request_token', {
+        method: 'POST',
+        body: JSON.stringify({
+            oauth_callback: encodeURI('https://influencer-tools.vercel.app/dashboard/post'),
+            oauth_consumer_key: process.env.NEXT_PUBLIC_TWITTER_API_KEY
+        })
+    });
+
+    const token_data = await req_token.json();
+
+    return NextResponse.json(token_data);
+}
--- components/Connect.tsx
+++ components/Connect.tsx
@@ -1,30 +1,21 @@
 'use client';
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
 export default function Connect({ name }: any) {
     const getURL = () => {
-        // let url =
-        //     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-        //     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-        //     'http://localhost:3000/'
-        // // Make sure to include `https://` when not localhost.
-        // url = url.includes('http') ? url : `https://${url}`
-        // // Make sure to include a trailing `/`.
-        // url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
-        // return url
         let url = process?.env?.NODE_ENV === 'production' ? 'https://influencer-tools.vercel.app/dashboard/post' : 'http://localhost:3000/dashboard/post';
         return url;
     }
 
-    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL ?? '', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '');
+    // const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL ?? '', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '');
 
     const handleConnectClick = (name: any) => {
         switch (name) {
             case 'Instagram':
-                // signInWithInstagram();
+                signInWithInstagram();
                 break;
             case 'TikTok':
-                // signInWithTikTok();
+                signInWithTikTok();
                 break;
             case 'Twitter':
                 signInWithTwitter();
@@ -33,13 +24,22 @@ export default function Connect({ name }: any) {
     }
 
     async function signInWithTwitter() {
-        const { data, error } = await supabase.auth.signInWithOAuth({
-            provider: 'twitter',
-            options: {
-                redirectTo: getURL(),
-            }
-        });
+        // const { data, error } = await supabase.auth.signInWithOAuth({
+        //     provider: 'twitter',
+        //     options: {
+        //         redirectTo: getURL(),
+        //     }
+        // });
+        // Step 1: post request token
+        const request = await fetch('https://influencer-tools.vercel.app/api/twitter/request');
+        const data = await request.json();
+        console.log(data);
+
     }
+
+    async function signInWithInstagram() {}
+
+    async function signInWithTikTok() {}
 
     return (
         <button onClick={() => { handleConnectClick(name) }} className="g-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Connect</button>
