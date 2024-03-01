e0406dfe06a49ea65e454c9b4b95808ee479218b
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-02-29 19:34:59 -0500

    twitter api

--- app/dashboard/post/page.tsx
+++ app/dashboard/post/page.tsx
@@ -8,45 +8,74 @@ export default function page() {
   const [firstGenerate, setFirstGenerate] = useState(false);
   const [userInput, setUserInput] = useState('');
 
-  let url = '';
-  useEffect(() => {
-    url = window.location.href;
-  }, []);
-  
-  let access_token = '';
-  let expires_at = '';
-  if (url.includes('access_token') && url.includes('expires_at')) {
-    access_token = url.split('access_token=')[1];
-    expires_at = url.split('expires_at=')[1];
+  function getToken() {
+    const url = window.location.href;
+    const token = url.split('=')[1].split('&')[0];
+    return token;
   }
 
   const getData = async () => {
     const API = process.env.NEXT_PUBLIC_API_URL;
+    const json = JSON.stringify({ text: userInput });
+
     const response = await fetch(`${API}/generate_post` as string, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
       },
-      body: JSON.stringify({ text: userInput })
+      body: json
     });
+
     const data = await response.json();
     return data;
   }
 
   const handleGenerateClick = () => {
     // Call your API here and set the text
-    getData()
-      .then(data => {
-        setText(data);
-        setShowTextField(true);
-        setFirstGenerate(true);
-      });
+    // getData()
+    //   .then(data => {
+    //     setText(data['data'][0]);
+    //     setShowTextField(true);
+    //     setFirstGenerate(true);
+    //   });
+
+    setText('This tweet was automatically generated using Cohere API (maybe)');
+    setShowTextField(true);
+    setFirstGenerate(true);
   };
 
   const handlePublishClick = () => {
+    tweet()
+      .then(data => {
+        console.log(data);
+      });
+  }
+
+  async function tweet() {
+    const API = 'https://api.twitter.com/2/tweets';
     // Call your publish function here
-    console.log('published text');
-  };
+    // post tweet to twitter using users access token
+    const token = getToken();
+    const auth = `Bearer ${token}`;
+
+    const json = JSON.stringify({ 'text': text });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Authorization': auth,
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: json
+    };
+
+    const response = await fetch(API as string, requestOptions);
+    const data = await response.json();
+    return data;
+  }
+
 
   return (
     <div className="w-full p-10">
